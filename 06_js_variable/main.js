9343cff16bd6b10ab096f59d2fe2540857e3516a
Richard Kempert <ritchyk@richards-mbp.home>
2020-03-21 21:47:26 +0100

    typeofoperator

--- 06_js_variable/main.js
+++ 06_js_variable/main.js
@@ -1,5 +1,4 @@
-let myName = 'Rudi';
-let myCity = 'IBK';
-
-console.log(`My name is ${myName}. My favorite city is ${myCity}.`)
-
+let newVariable = 'Playing around with typeof.';
+console.log(typeof newVariable);
+newVariable =1
+console.log(typeof newVariable)
